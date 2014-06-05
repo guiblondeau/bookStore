b70ed1fc221d910ee2d048842d023cb78a9f2bf8
Guillaume Blondeau <blondeau.gui@gmail.com>
2014-06-05 21:49:57 +0200

    Update test

--- js/newController.js
+++ js/newController.js
@@ -8,19 +8,23 @@ var testApp = angular.module('testApp', []);
 testApp.controller('newController',
     ['$scope', '$http',
         function($scope, $http){
-
+            $scope.test = "test"
             /*
              Get books
              */
             $http.get('../books/?strategy=load').success(function(data){
+                console.log("coucou1");
                 $scope.books =  data.list;
             }, function(data){
+                console.log("coucou2");
                 $scope.books =  data.list;
             });
 
             $http.get('../books/?strategy=load').success(function(data){
+                console.log("coucou3");
                 $scope.users =  data.list;
             }, function(data){
+                console.log("coucou4");
                 $scope.users =  data.list;
             });
 
